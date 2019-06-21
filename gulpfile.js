9463d703ebf15956701ee2bfab9dc931ef90b52b
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-06-21 16:22:16 +0000

    fixed gulp-sass on gruntfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-‘use strict’;
-var gulp = require(‘gulp’);
-var sass = require(‘gulp-sass’);
-// compile gulp.task(‘sass’, function () { gulp.src(‘app/scss/app.scss’) .pipe(sass().on(‘error’, sass.logError)) .pipe(gulp.dest(‘app/css’)); });
-//compile and watch gulp.task(‘sass:watch’, function() { gulp.watch(‘app/scss/app.scss’, [‘sass’]);});
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+
+
+gulp.task('styles', () => {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
