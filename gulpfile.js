d8ada8b7177d0da476e56a3dad4dac3a0a6e49f2
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-06-21 22:02:50 +0000

    Working on solving gulp issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const watch = require('gulp-watch');
 
 
 gulp.task('styles', () => {
-	return gulp.src('scss/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./css/'));
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
 });
+
+gulp.task('default', gulp.parallel('styles']);
