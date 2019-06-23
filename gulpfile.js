14ad35cde60d73c0573b236f716ca54cf7d5f3b5
HAuckram <hannnah.auckram@xtra.co.nz>
2019-06-23 21:08:00 +1200

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-
 gulp.task('styles', () => {
 	return gulp.src('scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css/'));
 });
+
+gulp.task('watch', function () {
+	gulp.watch('./**/*.scss', ['styles']);
+});
+gulp.task('watch', () => {
+	gulp.watch('scss/**/*.scss', (done) => {
+		gulp.series(['styles'])(done);
+	});
+});
+
+gulp.task('default', gulp.parallel('styles'));
