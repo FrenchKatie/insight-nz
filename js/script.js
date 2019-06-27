1dbed68d84caeea456c634e07b57887e41d08002
HAuckram <hannnah.auckram@xtra.co.nz>
2019-06-27 18:49:49 +1200

    bit messy but top bar toggle works

--- js/script.js
+++ js/script.js
@@ -1,7 +1,43 @@
 $('document').ready(function () {
 
 	$(".nav-item").click(function () {
+		var elem = $(this);
+		var dwn = false;
+		if (elem.hasClass("down")) {
+			dwn = true;
+		}
+		$(".nav-item").removeClass("down");
+		if (!dwn) {
+			elem.addClass("down"); //to close all other arrows when another is opened
+		}
 		event.preventDefault();
-		$(this).toggleClass("down");
 	});
+
+
+
+
+	$(".top-bar").mouseenter(function () {
+		var elem = $(this);
+		var dwn = false;
+		if (elem.hasClass("active")) {
+			//do nothing
+		} else {
+			$(".top-bar").removeClass("hover");
+			$(".top-bar").addClass("unactive");
+			elem.removeClass("unactive");
+			elem.toggleClass("hover");
+		}
+
+
+		event.preventDefault();
+	}).mouseleave(function () {
+		$(".top-bar").removeClass("hover");
+
+		$(".top-bar").removeClass("unactive");
+
+		event.preventDefault();
+	});
+
+
+
 });
