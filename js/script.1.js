f5f91f78722272fa053cf3c458c365eae9ec37b7
HAuckram <hannnah.auckram@xtra.co.nz>
2019-07-10 21:58:15 +1200

    open images on click

--- js/script.1.js
+++ js/script.1.js
@@ -1,14 +1,22 @@
 //js for adding images depending on what link is clicked
 
-$(".item").each(function (index) {
-
+$(".guideline").click(function () {
 	var elem = $(this);
-	var photograph = elem.find('.photograph');
-	var journal = elem.find('.journal');
-	var tag = elem.closest('.tag');
-
-	photograph.attr('src', 'assets/Images/Shoes/' + index + '.png');
-	journal.attr('src', 'assets/Images/Shoes/' + String.fromCharCode(97 + index) + '.png');
-
-	console.log(index + ": ");
+	var id = elem.attr("id");
+	showImages(id);
 });
+
+$(".insights-content").hide();
+
+function showImages(id) {
+	$(".insights-content").show();
+	$(".item").each(function (index) {
+		var elem = $(this);
+		var photograph = elem.find('.photograph');
+		var journal = elem.find('.journal');
+		var tag = elem.closest('.tag');
+
+		photograph.attr('src', 'assets/Images/Shoes/' + index + '.png');
+		journal.attr('src', 'assets/Images/Shoes/' + String.fromCharCode(97 + index) + '.png');
+	});
+}
