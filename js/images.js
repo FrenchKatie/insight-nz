ced18437d62ce217dd79a0992f70fa45b1a9bd31
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-07-15 10:59:30 +0000

    click sticky note to remove filter, only works once

--- js/images.js
+++ js/images.js
@@ -31,44 +31,69 @@ function showText(elem) {
 	$('.active-guideline-link-text').text(elem.text().trim());
 	$('.active-guideline-link').addClass("active");
 	$('#rotateBtn').show();
-
 }
 
 function showImages(id) {
 	$(".insights-content").show();
-	// $(".item:not(:first)").each(function (index) {
-
 	$(".item").each(function (index) {
 		var elem = $(this);
 		var photograph = elem.find('.photograph');
 		var journal = elem.find('.journal');
 		var tag = elem.closest('.tag');
 		var subtag = elem.closest('.subtag');
-
-		// id = 5;
-		console.log('id is = ' + id);
 		var file = 'assets/Images/' + id + '/';
-		console.log('file is = ' + file);
-		console.log('index is = ' + index);
-
 		photograph.attr('src', file + index + '.jpg');
 		journal.attr('src', file + String.fromCharCode(97 + index) + '.png');
-
 	});
 }
 
 $(".tag").click(function () {
 	var elem = $(this);
 	if (elem.closest('.item').hasClass("service-designer")) {
+		//if user clicks service-designer filter
+		console.log('service designer filter added');
 		$(".item-wrapper").attr('class', ' item-wrapper service-designer');
 		$(".sticky-note").attr('class', ' sticky-note service-designer');
 
+
 	} else if (elem.closest('.item').hasClass("ux-designer")) {
+			//if user clicks ux-designer filter
+		console.log('ux designer filter added');
 		$(".item-wrapper").attr('class', ' item-wrapper ux-designer');
 		$(".sticky-note").attr('class', ' sticky-note ux-designer');
+
 	} else if (elem.closest('.item').hasClass("typographer")) {
+			//if user clicks typographer filter
+		console.log('typographer filter added');
 		$(".item-wrapper").attr('class', ' item-wrapper typographer');
 		$(".sticky-note").attr('class', ' sticky-note typographer');
+	}
+});
+
+
+//removing sticky note filter
+$(".sticky-note").click(function () {
+	var elem = $(this);
+	console.log(elem);
+	console.log('sticky note click');
+	// $(".item-wrapper").removeClass( 'ux-designer');
+	// $(".sticky-note").removeClass( 'sticky-note');
+
+	if (elem.hasClass("service-designer")) {
+		console.log('service-designer  filter removed');
+		$(".item-wrapper").removeClass( 'service-designer');
+		$(".sticky-note").removeClass( 'sticky-note');
+
+
+	} else if (elem.hasClass("ux-designer")) {
+		console.log('ux designer filter removed');
+		$(".item-wrapper").removeClass( 'ux-designer');
+		$(".sticky-note").removeClass( 'sticky-note');
+
+	} else if (elem.hasClass("typographer")) {
+		console.log('typographer filter removed');
+		$(".item-wrapper").removeClass( 'typographer');
+		$(".sticky-note").removeClass( 'sticky-note');
 	}
 });
 
@@ -77,47 +102,42 @@ $(".subtag").click(function () {
 	var elem = $(this);
 	if (elem.closest('.item').hasClass("inhouse")) {
 		$(".item-wrapper").attr('class', ' item-wrapper inhouse');
-		$(".sticky-note").attr('class', ' sub-sticky-note inhouse');
+		$(".sub-sticky-note").attr('class', ' sub-sticky-note inhouse');
 		// console.log('inhouse subtag was clicked');
 	} else if (elem.closest('.item').hasClass("agency")) {
 		$(".item-wrapper").attr('class', ' item-wrapper agency');
-		$(".sticky-note").attr('class', ' sub-sticky-note agency');
+		$(".sub-sticky-note").attr('class', ' sub-sticky-note agency');
 		// console.log('agency subtag was clicked');
 	} else if (elem.closest('.item').hasClass("freelance")) {
 		$(".item-wrapper").attr('class', ' item-wrapper freelance');
-		$(".sticky-note").attr('class', ' sub-sticky-note freelance');
+		$(".sub-sticky-note").attr('class', ' sub-sticky-note freelance');
 		// console.log('freelance subtag was clicked');
 	}
 });
 
 $('.item-wrapper').on('scroll', function (index) {
-	// console.log('Working');
 	var imagePos = $('.item.one').offset().left;
 	var topOfWindow = $(window).scrollLeft();
 	var mrg = imagePos - topOfWindow;
 	if (mrg <= 1) {
 		// if mrg is less than 1 then it loses margin. Translate
-		// $('.insights-content').css('margin-left', 0 + 'px');
 		$('.insights-content').removeClass('scroller-margin');
-		$('.insights-content').addClass('no-scroller-margin');
-
-		// When the carousel hit the left wall and it is swiped to the right it need to gaint he margin back
-		console.log("margin-lost");
+		$('.insights-content').addClass('no-scroller-margin');// When the carousel hit the left wall and it is swiped to the right it need to gaint he margin back
+		// console.log("margin-lost");
 	} else if (mrg <= 600) {
 		//or if margin is less than 600, add margin.
-		console.log('else if');
-		// $('.insights-content').css('margin-left', 450 + 'px');
+		// console.log('else if');
 		$('.insights-content').removeClass('no-scroller-margin');
-		console.log('margin less than 600, no-scroller-margin class removed');
+		// console.log('margin less than 600, no-scroller-margin class removed');
 		$('.insights-content').addClass('scroller-margin');
-		console.log('margin less than 600, scroller-margin class add');
+		// console.log('margin less than 600, scroller-margin class add');
 
 
 	} else {
-		console.log('else');
+		// console.log('else');
 	}
 	if (mrg < topOfWindow) {
-		console.log('top of window');
+		// console.log('top of window');
 		$(this).delay(index * 600).queue(function () {
 			// $('.insights-content').css('margin-left', mrg + 'px')
 
