cf01b0ebf3487c96cec6eb02ae74ceaab5a77f11
HAuckram <hannnah.auckram@xtra.co.nz>
2019-07-16 20:53:03 +1200

    change grid to tabl
    have changed grid to table because it allows us to have tables within a table which is needed i think? so that its easier to add those sticky notes that should be sticky vertically. let me know if you can think of a better way of doing this like going back to the grid way of it

--- js/grid.js
+++ js/grid.js
@@ -1,29 +1,73 @@
-	$(".person").each(function (index) {
-		var elem = $(this);
-		var photograph = elem.find('.photograph');
-		var id = 5;
-		var file = 'assets/Images/' + id + '/';
+var table = $("<table>");
+var tableWrapper = $("<tr>");
 
-		photograph.attr('data-src', file + index + '.png');
+for (i = 1; i <= 5; i++) {
+	var col = $("<td>").addClass("column-wrapper");
+	var innertable = $("<table>");
 
+	for (j = 1; j <= 10; j++) {
+		var personWrapper = $("<tr>");
+		var person = $("<td>").addClass("person");
+		var imageFile = 'assets/Images/' + j + '/' + i + '.png';
+		var journalFile = 'assets/Images/' + j + '/' + String.fromCharCode(97 + i) + '.png';
+		var designer = getDesignerType(i);
+
+		var col = $('<td>').addClass(designer);
+		var person = $('<div>').addClass('person');
+		var journalWrapper = $('<div>').addClass('journal-wrapper');
+		var journal = '<img class = "journal" src = "' + journalFile + '" /> ';
+		var photoWrapper = $('<div>').addClass('photograph-wrapper');
+		var photo = '<img class = "lazy photograph" src="' + imageFile + '" />';
+		person.append(journalWrapper.append(journal));
+		person.append(photoWrapper.append(photo));
+
+		innertable.append(personWrapper).append(person);
+	}
+	tableWrapper.append(col).append(innertable);
+}
+
+table.append(tableWrapper);
+$('.grid').append(table);
+
+
+
+
+
+
+
+
+
+
+
+
+$('.person').on('click', clickToPerson);
+
+function clickToPerson(event) {
+	event.stopPropagation();
+	event.target.scrollIntoView({
+		behavior: "smooth",
+		block: "center",
+		inline: "center"
 	});
-	var i;
-	for (i = 0; i < 10; i++) {
-		var id = 5;
-		var imageFile = 'assets/Images/' + id + '/' + i + '.png';
-		var journalFile = 'assets/Images/' + id + '/' + String.fromCharCode(97 + i) + '.png';
-		//could make up a list of what ids need what tags...
+}
 
-		$('.grid').append(' <div class = "person"><div class="journal-wrapper"><img class="journal" src="' + journalFile + '"/></div> <div class="photograph-wrapper"><img class = "lazy photograph" src="' + imageFile + '" /></div></div>');
+function getDesignerType(index) {
+	switch (index) {
+		case 0:
+		case 3:
+		case 5:
+			return "ux-designer";
+		case 2:
+		case 4:
+		case 6:
+			return "service-designer";
+		case 1:
+			return "videographer";
+		case 7:
+			return "typographer";
+		case 8:
+			return "creative";
+		default:
+			return "unknown";
 	}
-
-	$('.person').on('click', clickToPerson);
-
-	function clickToPerson(event) {
-		event.stopPropagation();
-		event.target.scrollIntoView({
-			behavior: "smooth",
-			block: "center",
-			inline: "center"
-		});
-	}
+}
